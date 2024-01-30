7453dd864b188191b3181dceb67dc2de06d173af
SSDShehroz <shehroz@halimsol.com>
2024-01-30 18:50:26 +0500

    Customer Entity Confuguration Done

---
+++ Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+internal class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        //primary key
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Id).HasConversion
+            (
+                customerId => customerId.Id,
+                value => new CustomerId(value)
+            );
+
+        builder.Property(c => c.Name).HasMaxLength(100);
+
+        builder.Property(c => c.Email).HasMaxLength(255);
+        builder.HasIndex(c => c.Email).IsUnique();
+    }
+}
